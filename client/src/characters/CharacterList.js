bc3650b6107ab8d75e6742e97d732eacd67a0cae
koscida <britkos@gmail.com>
2020-07-05 13:14:14 -0400

    add group filter for characters

--- client/src/characters/CharacterList.js
+++ client/src/characters/CharacterList.js
@@ -1,19 +1,40 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import CharacterContext from '../contexts/CharacterContext'
 import EditingContext from '../contexts/EditingContext'
 import AddCharacter from './AddCharacter'
 import AddGroup from './AddGroup'
 
 import Form from 'react-bootstrap/Form'
-import { CaretDownFill } from 'react-bootstrap-icons';
+import Button from 'react-bootstrap/Button'
+import { CaretDownFill as ArrowIcon, XCircleFill as CancelIcon } from 'react-bootstrap-icons';
 
-function CharacterList(props) {
+const CharacterList = () => {
 	const { 
 		characters, selectedCharacterData, onCharacterSelect,
 		groups,
 	} = useContext(CharacterContext)
 	const { isEditing, setEditingErrorMessage } = useContext(EditingContext)
-	const [filterState, setFilterState] = useState(false)
+	const [filterState, setFilterState] = useState({
+		group: {
+			show: false,
+			active: false,
+			ids: {}
+		}
+	})
+	
+	useEffect( () => {
+		setFilterState({
+			...filterState,
+			group: {
+				...filterState.group,
+				ids: groups.reduce( (a,g) => {
+					a = {...a,[g.id]:false}
+					return a
+				}, {})
+			}
+		})
+	}, [groups])
+	
 	
 	
 	const handleCharacterSelect = (id) => {
@@ -21,36 +42,117 @@ function CharacterList(props) {
 		else onCharacterSelect(id)
 	}
 	
+	/*
+	 * Filters
+	 */
+	const toggleGroupFilter = () => {
+		setFilterState({
+			...filterState, 
+			group:{
+				...filterState.group, 
+				show: !filterState.group.show
+			}
+		})
+	}
+	
+	const handleGroupFilterSelect = e => {
+		let newIDs = {
+			...filterState.group.ids, 
+			[e.target.value] : !filterState.group.ids[e.target.value]
+		}
+		let newActive = !Object.values(newIDs).every(id => !id)
+		setFilterState({
+			...filterState, 
+			group:{
+				...filterState.group, 
+				active: newActive,
+				ids: newIDs
+			}
+		})
+	}
+	
+	const clearGroupFilter = () => {
+		setFilterState({
+			...filterState,
+			group: {
+				show: false,
+				active: false,
+				ids: groups.reduce( (a,g) => {
+					a = {...a,[g.id]:false}
+					return a
+				}, {})
+			}
+		})
+	}
+	
+	const filteredCharacters = () => {
+		return characters.filter( character => {
+			let activeFilterCount = 0
+			let charFilterCount = 0
+			Object.entries(filterState).forEach( ([filterType, filterData]) => {
+				if(filterData.active) {
+					if(filterType === "group") {
+						Object.entries(filterData.ids).forEach( ([id, idFilter]) => {
+							if(idFilter) {
+								activeFilterCount++
+								if(character.groupIds.includes(parseInt(id))) 
+									charFilterCount++
+							}
+						})
+					}
+				}
+			})
+			return (activeFilterCount === charFilterCount)
+		})
+	}
+	
 	return (
 		<div id="sidebarContent">
 			<div id="listFilter">
 				<div className="filter">
-					<div className="filterBtn" onClick={() => setFilterState(!filterState)}>
-						Group <CaretDownFill />
+					<div className="filterBtn" onClick={toggleGroupFilter}>
+						Group <span>{filterState.group.show?<CancelIcon />:<ArrowIcon />}</span>
 					</div>
-					{filterState
+					{filterState.group.show
 						? <div className="filterBox">
-							{groups.map( group => 
-								<Form.Group controlId={"filterCheckbox"+group.id}>
-									<Form.Check type="checkbox" label={group.name} />
-								</Form.Group>
-							)}
-						</div>
+								{groups.map( group => 
+									<Form.Group controlId={"filterCheckbox"+group.id} key={group.id}>
+										<div className="form-check">
+											<input 
+												type="checkbox" 
+												id={"filterCheckbox"+group.id}
+												className="form-check-input" 
+												value={group.id} 
+												checked={filterState.group.ids[group.id] || false}
+												onChange={handleGroupFilterSelect}
+											/>
+											<label 
+												htmlFor={"filterCheckbox"+group.id} 
+												className="form-check-label"
+											>{group.name}</label>
+										</div>
+									</Form.Group>
+								)}
+								<Button onClick={clearGroupFilter} size="sm" className="mr-2">Clear</Button>
+								<Button onClick={toggleGroupFilter} size="sm">Apply</Button>
+							</div>
 						: null
 					}
 				</div>
 			</div>
 			<div id="list">
 				<ul>
-					{characters.map( ({id, name}, idx) => {
-						return (
-							<li 
-								key={id} 
-								onClick={() => handleCharacterSelect(id)}
-								className={(selectedCharacterData && selectedCharacterData.id===id) ? 'active' : ''}
-							>{name}</li>
-						)
-					})}
+					{filteredCharacters()
+						.map( ({id, name}, idx) => {
+							return (
+								<li 
+									key={id} 
+									onClick={() => handleCharacterSelect(id)}
+									className={(selectedCharacterData && selectedCharacterData.id===id) ? 'active' : ''}
+								>{name}</li>
+							)
+						})
+					}
 				</ul>
 			</div>
 			<div id="listAdd">
